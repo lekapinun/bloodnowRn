345e48d0673ac3ebe1e556564e1695af8184e82c
au12113 <wasupont@windowslive.com>
2017-07-05 15:03:41 -0700

    update
    แก้ Countdown style ไม่ใช้ขนาดเป็น %

--- components/common/Countdown.js
+++ components/common/Countdown.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Font } from 'expo';
+import Colors from '../../constants/Colors';
 
-const Countdown = ({recentDonateDate}) => {
+const Countdown = ({recentDonateDate }) => {
   const timeRemaining = (7776000000 -( (new Date()) - (new Date(recentDonateDate))))
     / (24*60*60*1000);
   if( timeRemaining > 0) {
@@ -24,18 +25,18 @@ const Countdown = ({recentDonateDate}) => {
   }
 
   return (
-    <View style={{width: '80%', height: '50%', alignSelf: 'center'}}>
+    <View style={styles.countdownBorder}>
       <Text>
         นับถอยหลัง
       </Text>
         <View style={styles.countdownContainerStyle}>
         <View style={styles.countdownViewStyle}>
-          <Text>เดือน</Text>
           <Text style={[Font.style('CmPrasanmit'),styles.countdownTextStyle]}>{remainMonth}</Text>
+          <Text>เดือน</Text>
         </View>
         <View style={styles.countdownViewStyle}>
-          <Text>วัน</Text>
           <Text style={[Font.style('CmPrasanmit'),styles.countdownTextStyle]}>{remainDate}</Text>
+          <Text>วัน</Text>
         </View>
       </View>
       {countdownStatus}
@@ -44,6 +45,15 @@ const Countdown = ({recentDonateDate}) => {
 }
 
 const styles = StyleSheet.create({
+  countdownBorder: {
+    width: '80%',
+    height: 200,
+    alignSelf: 'center',
+    marginTop: 15,
+    borderWidth: 3,
+    borderRadius: 5,
+    borderColor: Colors.tabBar,
+  },
   countdownContainerStyle: {
     //backgroundColor: 'rgba(255, 77, 77,0.8)',
     flexDirection: 'row',
@@ -58,6 +68,8 @@ const styles = StyleSheet.create({
   countdownTextStyle: {
     alignSelf: 'center',
     fontSize: 55,
+    backgroundColor: Colors.tabBar,
+    color: 'white',
   },
 });
 
--- screens/DonorScreen.js
+++ screens/DonorScreen.js
@@ -20,14 +20,23 @@ export default class DonorScreen extends Component {
         headerStyle: {marginLeft:-250,backgroundColor: '#E84A5F'},
         gesturesEnabled: false,
     };
-
     state = {
-      onMoreDetail: false,
-      readyDonate: false,
-      list: {
-        title: "test",
-        thumbnail_image: "http://www.japanstyle.info/wordpress/wp-content/images/henohenomoheji.bmp"
-      },
+    countdownEnd: false,
+    onMoreDetail: false,
+    readyDonate: false,
+    list: {
+      title: "test",
+      thumbnail_image: "http://www.japanstyle.info/wordpress/wp-content/images/henohenomoheji.bmp"
+    },
+    }
+    componentWillReceiveProps(nextProps, nextState) {
+      if(nextProps.trigger){
+        this.setState(countdownEnd: true)
+      }
+    }
+
+    countdownTrigger = (e) => {
+      this.setState({ countdownEnd: e})
     }
 
     render() {
@@ -42,23 +51,23 @@ export default class DonorScreen extends Component {
                     <View
                       style={{ alignSelf: 'center', marginVertical: 150, width: 300, height: 300, backgroundColor: 'white' }}
                     >
-                    <Text>
-                      Hi there!
-                    </Text>
-                    <Text>{this.state.list.title}</Text>
-                    <TouchableOpacity
-                      style={{ position:'absolute', left: 0, right: 0, bottom: 0}}
-                      title="Back"
-                      onPress={ () => this.setState({ onMoreDetail: false})}
-                    >
-                      <Text style={{fontSize: 23, alignSelf: 'center'}}>Back</Text>
-                    </TouchableOpacity>
+                      <Text>
+                        Hi there!
+                      </Text>
+                      <Text>{this.state.list.title}</Text>
+                      <TouchableOpacity
+                        style={{ position:'absolute', left: 0, right: 0, bottom: 0}}
+                        title="Back"
+                        onPress={ () => this.setState({ onMoreDetail: false})}
+                      >
+                        <Text style={{fontSize: 23, alignSelf: 'center'}}>Back</Text>
+                      </TouchableOpacity>
 
                   </View>
 
               </Modal>
 
-              <Countdown recentDonateDate='12/2/17'/>
+              <Countdown recentDonateDate='12/2/16' />
 
               <View style={{alignSelf: 'center', flexDirection: 'row'}}>
                 <Text>คุณพร้อมบริจาคหรือไม่</Text>
@@ -68,7 +77,7 @@ export default class DonorScreen extends Component {
               <CardDetail
                 list={this.state.list}
                 onPress={() => this.setState({ onMoreDetail: true }) }
-                visible={this.state.readyDonate}
+                visible={this.state.countdownEnd}
               />
 
             </View>
