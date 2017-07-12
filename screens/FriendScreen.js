d9e9eca68c6002a24d5206183e3abab68368f5da
au12113 <wasupont@windowslive.com>
2017-07-12 17:30:41 +0700

    update 7/12/17

--- screens/FriendScreen.js
+++ screens/FriendScreen.js
@@ -11,12 +11,10 @@ import {
   AsyncStorage
 } from 'react-native';
 import { Font } from 'expo'
-
 import { TestButton, NavigatorBackground,ExNavigationState} from '../components/common';
 import { MonoText } from '../components/StyledText';
 import Colors from '../constants/Colors';
-
-
+import axios from 'axios';
 
 export default class FriendScreen extends Component {
     static navigationOptions =  {
@@ -26,22 +24,32 @@ export default class FriendScreen extends Component {
         headerStyle: {marginLeft:-250,backgroundColor: '#E84A5F'},
         gesturesEnabled: false,
     };
-    /*static route = {
-        navigationBar: {
-        title: 'เพื่อน',
-        backgroundColor: Colors.routeColor,
-        titleStyle: [Font.style('CmPrasanmitBold'),{fontSize:25}],
-        tintColor: 'white',
-        renderRight: () => <ExponentButton />,
-        },
-    };*/
+
+    state = {
+      list: [],
+    }
+
+    componentWillMount() {
+      axios.get("http://rallycoding.herokuapp.com/api/music_albums")
+      .then(response => this.setState({ list: response.data }));
+    }
+
+    renderList() {
+      //console.log(this.state.list)
+       return this.state.list.map(list =>
+         <View
+           key = {list.title}
+           style={{ flex: 0.3, height: 80, width: 80, backgroundColor: 'brown', margin: 10}}
+         />
+         //<CardDetail key={list.title} list={list} visible={true}/>
+       );
+     }
 
     render() {
-        return(
-            <View style={{marginTop:30}}>
-                <Text>FRIENDS SCREEN</Text>
-            </View>
-        );
-    }
+      return(
+        <View style={{backgroundColor: 'black', flexDirection: 'row', flex: 1, width: 350, height: 200}}>
+          {this.renderList()}
+        </View>
+    )};
 
 }
