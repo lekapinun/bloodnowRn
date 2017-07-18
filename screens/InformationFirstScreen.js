af71dbc7253512efaa105c347d91ba309bd34f26
au12113 <wasupont@windowslive.com>
2017-07-18 18:11:43 +0700

    เพิ่มคำแนะนำหน้าแรก

--- screens/InformationFirstScreen.js
+++ screens/InformationFirstScreen.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import {
-  Text,
+  Image,
   View,
   TouchableOpacity,
+  Dimensions,
 } from 'react-native';
 import { Font } from 'expo'
 
 export default class InformationFirstScreen extends Component {
     static navigationOptions =  {
-        title: 'คำแนะนำ',
+        title: 'ขั้นตอน',
         headerTintColor: 'white',
         headerTitleStyle: [Font.style('CmPrasanmitBold'),{fontSize:29}],
         headerStyle: {marginLeft:-250,backgroundColor: '#E84A5F'},
@@ -16,8 +17,11 @@ export default class InformationFirstScreen extends Component {
     };
     render() {
         return(
-            <View style={{marginTop:30}}>
-                <Text>INFORMATION1 SCREEN</Text>
+            <View style={{backgroundColor: 'white', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <Image
+                  style={{height: '90%', width: '90%'}}
+                  source={require('../assets/images/step_donate.png')}
+                />
             </View>
         );
     }
